1f37fa494876734d2b8f8b520cf21b270103b37c
dhiabziz <dhiditabdi@gmail.com>
2024-02-11 17:34:54 +0700

    testing

---
+++ src/program.py
@@ -0,0 +1,10 @@
+#Daftar fungsi
+#Input matriks manual, Input matriks random, Matrix to solusi, 
+
+width = 3
+height = 3
+matrix = [['A7', 'BD', 'C5'],['C5', 'A7', 'BD'],['A6', 'BD', 'C5']]
+print(matrix[0][0])
+
+for i in range (width):
+    print(i)
